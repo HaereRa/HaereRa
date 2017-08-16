8e2d4beaab4cce6a470bc696add7a066887987ec
Matt Cotterell <matt@cotterell.net.nz>
2017-08-16 17:03:10 +1200

    Resolve cyclic dependencies in DI

--- src/HaereRa.API/Services/PersonService.cs
+++ src/HaereRa.API/Services/PersonService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using HaereRa.API.Models;
-using System.Collections.Generic;
+﻿using HaereRa.API.Models;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Linq;
@@ -11,15 +9,11 @@ namespace HaereRa.API.Services
 {
     public class PersonService : IPersonService
     {
-        private readonly ISuggestionService _suggestionService;
-        private readonly IProfileService _profileService;
         private readonly HaereRaDbContext _dbContext;
 
-        public PersonService(HaereRaDbContext dbContext, IProfileService profileService, ISuggestionService suggestionService)
+        public PersonService(HaereRaDbContext dbContext)
         {
             _dbContext = dbContext;
-            _profileService = profileService;
-            _suggestionService = suggestionService;
         }
 
         public async Task<Person> GetPersonAsync(int id, CancellationToken cancellationToken = default(CancellationToken))
@@ -35,46 +29,6 @@ namespace HaereRa.API.Services
 				.Where(p => p.Id == id)
 				.SingleOrDefaultAsync();
             return result;
-        }
-
-        public async Task<IReadOnlyDictionary<int, IEnumerable<string>>> GetProfileSuggestionsAsync(Person person, List<ProfileType> profileTypes, CancellationToken cancellationToken = default(CancellationToken))
-        {
-            if (person == null) throw new ArgumentNullException(nameof(person));
-            if (profileTypes == null || !profileTypes.Any()) throw new ArgumentNullException(nameof(profileTypes));
-            cancellationToken.ThrowIfCancellationRequested();
-
-            var possibleUsernameMatches = await _suggestionService.GetPossibleUsernamesAsync(
-                fullName: person.FullName,
-                knownAs: person.KnownAs,
-                dashesAllowed: false,
-                dotsUsed: false, 
-                cancellationToken: cancellationToken
-            );
-
-            var suggestionsList = new Dictionary<int, IEnumerable<string>>();
-            foreach (var profileType in profileTypes)
-            {
-                var profileSuggestions = new List<string>();
-
-                var allProfiles = await _profileService.GetAllProfilesForTypeAsync(profileType);
-                foreach (var profile in allProfiles)
-                {
-                    var trimmedUsername = profile.ProfileAccountIdentifier
-                        .Trim(new char[] { ' ', '_', '-', '.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' })
-                        .ToLower();
-                    if (possibleUsernameMatches.Contains(trimmedUsername))
-                    {
-                        profileSuggestions.Add(profile.ProfileAccountIdentifier);
-                    }
-                }
-
-                if (profileSuggestions.Any())
-                {
-                    suggestionsList.Add(profileType.Id, profileSuggestions);
-                }
-            }
-
-            return suggestionsList;
         }
     }
 
